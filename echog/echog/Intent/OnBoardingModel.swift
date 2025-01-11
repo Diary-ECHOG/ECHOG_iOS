8e66ee05ae899ad372e46fc55c071abf13217e4d
mint3382 <mskim0926@naver.com>
2025-01-12 00:38:17 +0900

    ✨(feat) OnBoardingModel 구현

---
+++ echog/echog/Intent/OnBoardingModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnBoardingModel.swift
+//  echog
+//
+//  Created by minsong kim on 1/12/25.
+//
+
+import UIKit
+
+struct OnBoardingModel {
+    let image: UIImage
+    let title: String
+    let isStartButton: Bool
+}
