4da0918efc25d22a370f0af84affb8e679014100
mint3382 <mskim0926@naver.com>
2025-01-13 14:54:17 +0900

    ✨(feat) Coordinator protocol 구현

---
+++ echog/echog/Coordinator/Coordinator.swift
@@ -0,0 +1,26 @@
+//
+//  Coordinator.swift
+//  echog
+//
+//  Created by minsong kim on 1/12/25.
+//
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var children: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+    func start()
+}
+
+extension Coordinator {
+    func childDidFinish(_ coordinator: Coordinator) {
+        for (index, child) in children.enumerated() {
+            if child === coordinator {
+                children.remove(at: index)
+                break
+            }
+        }
+    }
+}
