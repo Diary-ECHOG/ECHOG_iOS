f9434ed103b684dc7f28447913eb64fa52ac1bc8
mint3382 <mskim0926@naver.com>
2025-01-24 23:08:58 +0900

    ✨(feat) DiaryViewerIntent, State 구현

---
+++ echog/echog/Presentation/Intent/DiaryViewer/DiaryViewerState.swift
@@ -0,0 +1,13 @@
+//
+//  DiaryViewerState.swift
+//  echog
+//
+//  Created by minsong kim on 1/13/25.
+//
+
+enum DiaryViewerState {
+    case back
+    case detail
+    case fix
+    case remove
+}
