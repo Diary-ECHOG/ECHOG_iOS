ac24eac0bc7fce1cb058cf9bd66777efb2ad564e
mint3382 <mskim0926@naver.com>
2025-02-07 00:55:31 +0900

    ✨(feat) UserRegisterNetworkBuilder, register() 구현

---
+++ echog/echog/Data/User/Builder/UserRegisterNetworkBuilder.swift
@@ -0,0 +1,21 @@
+//
+//  UserRegisterNetworkBuilder.swift
+//  echog
+//
+//  Created by minsong kim on 2/6/25.
+//
+
+import Foundation
+
+struct UserRegisterNetworkBuilder: NetworkBuilderProtocol {
+    typealias Response = RegisterDTO
+    
+    var baseURL: BaseURLType { .api }
+    var path: String { "/api/users/register" }
+    var queries: [URLQueryItem]? = nil
+    var method: HTTPMethod { .post }
+    let parameters: [String: Any]
+    let deserializer: NetworkDeserializable = JSONNetworkDeserializer(decoder: JSONDecoder())
+
+    var useAuthorization: Bool { false }
+}
