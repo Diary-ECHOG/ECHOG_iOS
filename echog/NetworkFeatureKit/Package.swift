10f61e3906590fc299a68d34e5f8257be8fe7096
mint3382 <mskim0926@naver.com>
2025-02-20 13:28:43 +0900

    🐞(fix) DiaryFeatureKit dependency 오류 수정

--- echog/NetworkFeatureKit/Package.swift
+++ echog/NetworkFeatureKit/Package.swift
@@ -10,13 +10,19 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkFeatureKit",
-            targets: ["NetworkFeatureKit"]),
+            targets: ["NetworkFeatureKit"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "NetworkKit", path: "../NetworkKit")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkFeatureKit"),
+            name: "NetworkFeatureKit",
+            dependencies: ["NetworkKit"]
+        ),
         .testTarget(
             name: "NetworkFeatureKitTests",
             dependencies: ["NetworkFeatureKit"]
